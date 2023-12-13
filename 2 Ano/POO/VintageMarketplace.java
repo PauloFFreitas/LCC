ac9bfe6b7fa1c099ab2c31636d97269df16c102a
Paulo Freitas <99907305+PauloFFreitas@users.noreply.github.com>
2023-12-13 23:00:16 +0000

    Add files via upload

---
+++ 2 Ano/POO/VintageMarketplace.java
@@ -0,0 +1,16 @@
+package Projeto;
+
+import Projeto.View.Menu;
+
+public class VintageMarketplace {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.run();
+    }
+}
+
+
+
+
+
+
